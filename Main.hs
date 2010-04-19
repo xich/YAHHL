75edc23ded11fcc9d1b6ed7650e162da61cc89d2
Andrew Farmer <anfarmer@ku.edu>
2010-04-19 17:03:15 -0500

    Better syntax and typeclass instances.

--- Main.hs
+++ Main.hs
@@ -1,12 +1,17 @@
-{-# LANGUAGE ExistentialQuantification, TypeSynonymInstances, FlexibleInstances, TypeFamilies #-}
+{-# LANGUAGE MultiParamTypeClasses, FunctionalDependencies, UndecidableInstances, FlexibleContexts, FlexibleInstances, OverlappingInstances, TypeFamilies #-}
 
-module HTreeML where
+module Main where
 
 import Criterion.Main
 import qualified Text.Html as H
-import qualified Data.Text as T
+import qualified Data.Text.Lazy as T
 
--- data Tag = forall a . (Render a) => Tag T.Text [Attribute] a
+-- Tried to do the 'appending trick' (think ShowS) but found it to actually
+-- be slower... which makes sense. Data.Text.append makes a new
+-- array and copies each string into it, so we don't really gain anything
+-- by being clever.
+
+-- data Tag = forall a . (HTML a) => Tag T.Text [Attribute] a
 data Tag = Tag T.Text [Attribute] [Tag]
          | Text T.Text
          | EmptyTag
@@ -16,41 +21,49 @@ instance Show Tag where
 
 data Attribute = Empty
                | Align T.Text
+               | Href T.Text
 
-class Render a where
+class HTML a where
     render :: a -> T.Text
     store  :: a -> [Tag]
 
-instance Render Tag where
-    render (Tag n a c) = T.concat [open, n, render a, end, render c, close, n, end]
+-- For multiple children
+instance (HTML a) => HTML [a] where
+    render ts = T.concat $ map render ts
+    store  ts = concatMap store ts
+
+-- Single tag
+instance HTML Tag where
+    render (Tag n a c) = T.concat [open, n, renderA a, end, render c, close, n, end]
     render (Text s) = s
     render EmptyTag = T.empty
 
     store t = [t]
 
--- goddamn overlapping instances bullshit (see String instance)
--- instance (Render a) => Render [a] where
---     render ts = T.concat $ map render ts
-
-instance Render [Tag] where
-    render ts = T.concat $ map render ts
-
-    store ts = ts
-
-instance Render [Attribute] where
-    render as = T.concat $ map render as
-    store = error "called store on list of attributes"
-
-instance Render Attribute where
+{-- Single attribute
+instance HTML Attribute where
     render Empty = T.empty
     render (Align dir) = T.concat [T.pack $ " align=\"", dir, endQuote]
+    render (Href url)  = T.concat [T.pack $ " href=\"", url, endQuote]
     store = error "called store on an attribute"
+-}
+renderA as = T.concat $ map r as
+    where
+        r Empty = T.empty
+        r (Align dir) = T.concat [T.pack $ " align=\"", dir, endQuote]
+        r (Href url)  = T.concat [T.pack $ " href=\"", url, endQuote]
 
-instance Render String where
+-- For Text
+instance HTML [Char] where
     render = T.pack
     store s = [Text $ T.pack s]
 
-instance (Render a, a ~ Tag) => Render (a -> Tag) where
+instance HTML T.Text where
+    render = id
+    store t = [Text t]
+
+-- So we can do empty tag groups, like: html body => <html><body></body></html>
+instance (HTML a, a ~ Tag) => HTML (a -> Tag) where
     render f = error "called render on a function"
     store f = [f EmptyTag]
 
@@ -59,36 +72,53 @@ close = T.pack "</"
 end = T.pack ">"
 endQuote = T.pack "\""
 
--- really would like to be able to take either a Tag, a [Tag], or a String as
--- the third argument. Can't get the type system to let me though.
--- seems like I should be able to do:
--- tag :: (Render a) => String -> [Attribute] -> a -> Tag
--- and modify the data declaration above to:
--- data Tag = forall a . (Render a) => Tag T.Text [Attribute] a
--- tag :: String -> [Attribute] -> [Tag] -> Tag
-tag :: (Render a) => String -> [Attribute] -> a -> Tag
+tag :: (HTML a) => String -> [Attribute] -> a -> Tag
 tag n a c = Tag (T.pack n) a (store c)
 
-text :: String -> Tag
-text s = Text (T.pack s)
+{-
+class TagContent a b | a -> b where
+    mkTag :: (HTML b) => String -> a -> b
 
+instance (HTML a) => TagContent [Attribute] (a -> Tag) where
+    mkTag name as = tag name as
+
+instance (HTML c) => TagContent c Tag where
+    mkTag name cs = tag name [] cs
+
+simple3 = mkTag "html" (tag "body" [] [tag "h1" [] "something", tag "p" [align "right"] "blah"])
+-}
 align = Align . T.pack
+href  = Href  . T.pack
 
-simple = tag "html" [] [tag "body" [] [tag "h1" [] [text "something"], tag "p" [align "right"] [text "blah"]]]
+simple = tag "html" [] [tag "body" [] [tag "h1" [] "something", tag "p" [align "right"] "blah"]]
 simple2 = html $ body [ h1 "something"
                       , p_a [align "right"] "blah"]
 
-main = defaultMain [ bench "empty" $ nf render $ html body
+main = defaultMain [ bench "empty"     $ nf render $ html $ body
                    , bench "old-empty" $ nf H.renderHtml $ H.body H.noHtml
-                   , bench "simple" $ nf render $ simple
-                   , bench "simple2" $ nf render $ simple2
+                   , bench "simple"    $ nf render $ simple
+                   , bench "simple2"   $ nf render $ simple2
+                   , bench "oldruby"   $ nf H.renderHtml $ (H.header $ H.thetitle $ H.primHtml "happy title")
+                                                           H.+++
+                                                           (H.body ((H.h1 $ H.primHtml "happy heading")
+                                                                   H.+++
+                                                                   H.hotlink "url" [H.primHtml "something"]
+                                                                   ))
+                   , bench "ruby"      $ nf render $ html [ head_ $ title "happy title"
+                                                          , body [ h1 "happy heading"
+                                                                 , a [href "url"] "a link"
+                                                                 ]
+                                                          ]
                    ]
 
-html :: (Render a) => a -> Tag
+html :: (HTML a) => a -> Tag
 html = tag "html" []
-body :: (Render a) => a -> Tag
+head_ = tag "head" []
+body :: (HTML a) => a -> Tag
 body = tag "body" []
 h1   = tag "h1"   []
-p :: (Render a) => a -> Tag
+p :: (HTML a) => a -> Tag
 p    = tag "p"    []
 p_a  = tag "p"
+a    = tag "a"
+title = tag "title" []
